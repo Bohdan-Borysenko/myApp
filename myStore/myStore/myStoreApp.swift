2b003d3900efaa26fa0c9bb7e5e293f322dc8594
Bohdan <borysenkobohdan@gmail.com>
2023-06-25 23:21:54 +0300

    1
    empty project

---
+++ myStore/myStore/myStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  myStoreApp.swift
+//  myStore
+//
+//  Created by Богдан Борисенко on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct myStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
